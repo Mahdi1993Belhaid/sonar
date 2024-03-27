aa6df573990481e55d7b0405a05b6e9b5efe9ff8
belhaid mahdi <dzbelhaidmahdi@gmail.com>
2024-03-27 07:23:39 +0100

    refactor code

--- src/main/java/com/example/tddproject/parser/Parser.java
+++ src/main/java/com/example/tddproject/parser/Parser.java
@@ -14,6 +14,8 @@ import java.util.logging.Logger;
 public class Parser {
 
 
+    private static final String FILE_PATH = "C:\\Users\\windows\\Desktop\\test.xml";
+
     public static void main(String[] args) {
 
         try {
@@ -22,7 +24,7 @@ public class Parser {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser parser = factory.newSAXParser();
 
-            File file = new File("C:\\Users\\windows\\Desktop\\test.xml");
+            File file = new File(FILE_PATH);
             parser.parse(file,new HandlerBase());
         }catch(Exception exception){
            System.out.println(exception.getMessage());
