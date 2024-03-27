109b692688138612d26c181f8dc05d205295d6d4
belhaid mahdi <dzbelhaidmahdi@gmail.com>
2024-03-27 08:31:44 +0100

    refactor code

--- src/main/java/com/example/tddproject/parser/Constants.java
+++ src/main/java/com/example/tddproject/parser/Constants.java
@@ -1,5 +1,7 @@
 package com.example.tddproject.parser;
 
 public class Constants {
+
+    private Constants(){};
     public  static final String FILE_PATH = "C:\\Users\\windows\\Desktop\\test.xml";
 }
