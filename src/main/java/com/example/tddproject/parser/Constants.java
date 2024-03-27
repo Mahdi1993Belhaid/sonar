0dd686eb595722243a35033418e3e97d1f2e1b59
belhaid mahdi <dzbelhaidmahdi@gmail.com>
2024-03-27 08:29:34 +0100

    refactor code

---
+++ src/main/java/com/example/tddproject/parser/Constants.java
@@ -0,0 +1,5 @@
+package com.example.tddproject.parser;
+
+public class Constants {
+    public  static final String FILE_PATH = "C:\\Users\\windows\\Desktop\\test.xml";
+}
--- src/main/java/com/example/tddproject/parser/Parser.java
+++ src/main/java/com/example/tddproject/parser/Parser.java
@@ -1,30 +1,31 @@
 package com.example.tddproject.parser;
 
 
-import org.apache.xerces.xni.XMLDTDHandler;
-import org.slf4j.LoggerFactory;
+
 import org.xml.sax.HandlerBase;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
-import java.util.logging.Logger;
+import java.lang.module.Configuration;
+
 
 public class Parser {
 
 
-    private static final String FILE_PATH = "C:\\Users\\windows\\Desktop\\test.xml";
+
 
     public static void main(String[] args) {
-
+        Parser parser= null ;
+        Configuration configuration;
         try {
+
 
             System.setProperty("javax.xml.parsers.SAXParserFactory", "com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl");
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser parser = factory.newSAXParser();
 
-            File file = new File(FILE_PATH);
+            File file = new File(Constants.FILE_PATH);
             parser.parse(file,new HandlerBase());
         }catch(Exception exception){
            System.out.println(exception.getMessage());
