56bff5fd92c0654e54766a54d2308d8b82df07f4
belhaid mahdi <dzbelhaidmahdi@gmail.com>
2024-03-27 06:56:28 +0100

    add test.xml file

--- src/main/java/com/example/tddproject/parser/Parser.java
+++ src/main/java/com/example/tddproject/parser/Parser.java
@@ -1,11 +1,14 @@
 package com.example.tddproject.parser;
 
 
+import org.apache.xerces.xni.XMLDTDHandler;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.HandlerBase;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
 import java.util.logging.Logger;
 
 public class Parser {
@@ -13,14 +16,14 @@ public class Parser {
 
     public static void main(String[] args) {
 
-
-
         try {
 
             System.setProperty("javax.xml.parsers.SAXParserFactory", "com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl");
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser parser = factory.newSAXParser();
 
+            File file = new File("C:\\Users\\windows\\Desktop\\test.xml");
+            parser.parse(file,new HandlerBase());
         }catch(Exception exception){
            System.out.println(exception.getMessage());
         }
